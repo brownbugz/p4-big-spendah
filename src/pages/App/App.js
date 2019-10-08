40956761795a8b03c41f7678974ae3e624ad4a8e
sonniakemmer <brownbugz@gmail.com>
2019-10-08 14:58:31 -0700

    adds handleSignupOrLogin function

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import {Route, Switch} from 'react-router-dom';
 import ExpenseBoardPage from '../ExpenseBoardPage/ExpenseBoardPage';
+import userService from '../../utils/userService';
 
 
 class App extends Component {
@@ -17,7 +18,9 @@ class App extends Component {
     this.setState({user: null});
   }
 
-  
+  handleSignupOrLogin = () => {
+    this.setState({user: userService.getUser()});
+  }
 
   render() {
     return (
@@ -27,7 +30,22 @@ class App extends Component {
         </header>
         <Switch>
           <Route exact path="/" render={() =>
-            <ExpenseBoardPage />
+            <ExpenseBoardPage 
+              user={this.state.user}
+              handleLogout={this.handleLogout}
+            />
+          }/>
+          <Route exact path="signup" render={({history}) =>
+            <SignupPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          }/>
+          <Route exact path="/login" render={({history}) =>
+            <LoginPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
           }/>
         </Switch>
       </div>
