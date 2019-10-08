32c6d3dc113c3f9b95dddf6c418ea86981458f3d
sonniakemmer <brownbugz@gmail.com>
2019-10-08 14:10:22 -0700

    adds mongoose and server.js

--- src/components/ExpenseCard/ExpenseCard.jsx
+++ src/components/ExpenseCard/ExpenseCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const ExpenseCard = (props) => {
-    <div>
-        <ul>
-            <li>Item: <input type="String"></input></li>
-            <li>Price: <input type="Number"></input></li>
-        </ul>
-    </div>
+    return (
+        <div>
+            <ul>
+                <li>Item: <input type="String"></input></li>
+                <li>Price: <input type="Number"></input></li>
+            </ul>
+        </div>
+    )
 }
 
 export default ExpenseCard;
