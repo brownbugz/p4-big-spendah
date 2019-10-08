ee895e72b735b45ca2ae4531c577667206f91155
sonniakemmer <brownbugz@gmail.com>
2019-10-08 12:51:10 -0700

    codes NavBar, imports Link

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 
 const NavBar = (props) => {
-    
+    let nav = props.user ?
+        <div>
+            <Link>Expense List</Link>
+            &nbsp;&nbsp;|&nbsp;&nbsp;
+            <Link>Logout</Link>
+            &nbsp;&nbsp;|&nbsp;&nbsp;
+            <span>Welcome</span>
+        </div>
+        :
+        <div>
+            <Link>Log In</Link>
+            &nbsp;&nbsp;|&nbsp;&nbsp;
+            <Link>Sign Up</Link>
+        </div>;
 
     return (
         <div className="NavBar">
-            nav
+            {nav}
         </div>
-
-    )
-}
+    );
+};
 
 export default NavBar;
