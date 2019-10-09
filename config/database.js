ab10871e8b56369d33545451d8df7265d2d78c25
sonniakemmer <brownbugz@gmail.com>
2019-10-08 17:44:25 -0700

    adds users in server.js and revises mongoose.connect in database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,19 @@
 var mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
 // shortcut to mongoose.connection object
 var db = mongoose.connection;
 
 db.once('connected', () => {
     console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
   });
+
+  mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
